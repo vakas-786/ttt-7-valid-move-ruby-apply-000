fcd511761ebbeaa664dd00101d627053766cf1f7
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-19 02:39:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move(board,index)
-  if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
+  if (board[index]) == " " || (board[index]) == " " || (board[index]) == nil
     return false
   else
     return true
