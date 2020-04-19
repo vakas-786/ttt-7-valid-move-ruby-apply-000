db775adbee075928294c4fd1edc75bac1150e02d
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-19 02:41:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,14 +8,10 @@ def valid_move(board,index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index)
-  if (board[index]) == " "
-    false
-  elsif (board[index]) == ""
-    false
-  elsif (board[index]) == nil
-    false
-  elsif (board[index]) == "X" || "O"
-    true
+def position_taken?(board, index)
+  if (board[index]) == " " || (board[index]) == "" || (board[index]) == nil
+    return false
+  else
+    return true
   end
 end
