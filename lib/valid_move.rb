82de88a841dffdc1fc358038c9c000e9dabec9b2
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-19 02:24:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,8 @@
 def valid_move
   if position_taken?
     execute something
+  else execute somethingelse
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
