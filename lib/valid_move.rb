d0126168542a4b558a1cd77dab9ac7e8655b7151
Vakas  Akhtar <vakhtar@fordham.edu>
2020-04-19 02:43:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,8 +6,6 @@ def valid_move(board,index)
           return false
         end
 
-end
-
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
